337b2815943cff1d483e661ac0c3755b38b1fead
mamichbogdan92-tech <mamichbogdan92@gmail.com>
2026-03-12 23:55:15 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,21 @@
+const models = [
+
+{
+name:"BarnHouse",
+price:24500,
+image:"images/barnhouse.jpg"
+},
+
+{
+name:"Scandinavia",
+price:24000,
+image:"images/scandinavia.jpg"
+},
+
+{
+name:"Byannon",
+price:23500,
+image:"images/byannon.jpg"
+}
+
+]
